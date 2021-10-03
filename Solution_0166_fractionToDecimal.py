ddab8f5fdc418407cfd5bce1ba7e738124c238ed
qizhenkang <qizhenkang@sina.com>
2021-10-03 13:12:07 +0800

    Update Solution_0166_fractionToDecimal.py

--- Solution_0166_fractionToDecimal.py
+++ Solution_0166_fractionToDecimal.py
@@ -9,34 +9,36 @@ class Solution:
     def fractionToDecimal(self, numerator: int, denominator: int) -> str:
         """
         第一反应感觉循环不太好处理，偷看了一眼答案，需要“小学除法”，大概有思路了
-        
+        是一个长除法，本质上是记录 商 和 余数
         """
+        # 符号处理
         minus = '-' if numerator * denominator < 0 else ''
         numerator = abs(numerator)
         denominator = abs(denominator)
         result = minus + str(numerator // denominator)
-
+        
+        # 判断是否需要处理小数，有没有整除啥的
         if numerator % denominator:
             result += '.'
-        # else:
-        #     return result
-        
+
         numerator = 10 * (numerator % denominator)
-        
-        print(result,numerator)
-        
+
+        # 进行小数判断
         bit = 0
         quotient = 0
         numeratorCycle = []
         quotientCycle = []
         Decimal = ''
+        # 长除法
         while numerator and bit < 1e4:
+            # 计算 商 和 余数
             if numerator > denominator:
                 quotient = numerator // denominator
                 numerator -= quotient * denominator
             else:
                 quotient = 0
-
+                
+            # 判断是否存在循环，即余数是否出现过
             print(numerator,denominator,quotient,numeratorCycle,quotientCycle,Decimal)
             if numerator in numeratorCycle:
                 posi = numeratorCycle.index(numerator)
@@ -48,6 +50,7 @@ class Solution:
                     Decimal += str(quotient)
                     Decimal = Decimal[:posi] +'('+ Decimal[posi:] + ')'
                 break
+            # 存储余数和商
             numeratorCycle.append(numerator)
             quotientCycle.append(quotient)
             Decimal += str(quotient)
