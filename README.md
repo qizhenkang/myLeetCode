fab5ef2c97b4feeaef0fa79258ef32cb692246c9
qizhenkang <qizhenkang@sina.com>
2021-10-04 16:42:36 +0800

    ReadMe File

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # myLeetCode
 
-This Zhenkang's LeetCode Repository by Python.
+This Zhenkang's LeetCode Repository(mainly by _Python_).
+
+## My Coding Record
+
+When I entered my Ph.D. career, I found that what I love is not researching, but programming.
+Therefore, I decided to start the journey of _LeetCode_, as a little source of happiness in my boring rearch life.
+
+| Number |         Name         | Difficulty | State |    Time    | Note |
+| :----: | :------------------: | :--------: | :---: | :--------: | :--: |
+|  0482  | licenseKeyFormatting |    Easy    |  AC   | 2020/10/04 |  ★☆  |
--- SolutionReName.py
+++ SolutionReName.py
@@ -5,7 +5,8 @@ Created on Sat Sep 25 15:58:46 2021
 @author: qizhe
 """
 
-import os ,os.path
+import os, os.path
+
 
 def rename(file):
     ''' file: 文件路径    keyWord: 需要修改的文件中所包含的关键字 '''
@@ -15,8 +16,10 @@ def rename(file):
     print(os.getcwd())
     for name in items:
         # print(name)
-        if name[:4] =='Solu':
-            if name[-2:] == 'py' or name[-2:] == 'pp' :
+        if name[:4] == 'Solu':
+            if name == 'SolutionReName.py':
+                continue
+            if name[-2:] == 'py' or name[-2:] == 'pp':
                 print(name)
                 new_name = name[:9] + '0' + name[9:]
                 print(new_name)
@@ -27,10 +30,11 @@ def rename(file):
         #         os.renames(name,new_name)
         # else:
         #     rename(file + '\\' + name, keyword)
-        #     os.chdir('...')      
+        #     os.chdir('...')
     # print('-----------------------分界线------------------------')
     # items = os.listdir(file)
     # for name in items:
-        # print(name)
- 
+    # print(name)
+
+
 rename('E:\\Python_Projects\\LeetCode')
