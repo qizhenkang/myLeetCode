e94b36490fad0af7bbb6c722f2c7dfb9d5e885e9
qizhenkang <qizhenkang@sina.com>
2021-10-24 22:46:27 +0800

    new solution

--- code/Solution_0099_recoverTree.py
+++ code/Solution_0099_recoverTree.py
@@ -17,10 +17,46 @@ class Solution:
         Do not return anything, modify root in-place instead.
         
         第一反应还是中序遍历，然后判断顺序。
+        
+        二次读题：
+        1、感觉像是一个二叉树的排序算法，要求不改变结构，就很像中序遍历+冒泡排序
+        2、尝试一下递推栈，没那么简单，感觉不好下手
+        3、思路上，就是二叉树的排序，需要中序遍历，需要交换位置
+        4、只有两个节点被交换位置了，应该还简单一点
         """
         
-        
+        def __inorder(root):
+            
+            if root.left:
+                __inorder(root.left)
+            
+            # print(root.val)
+            # 先只交换值试试
+            if root.right:
+                __inorder(root.right)
+            
+            return 
+        # __inorder(root)
         return
+        # stack = [root]
+        # while stack:
+        #     for i in range(len(stack)):
+        #         node = stack.pop()
+        #         if node.right:
+        #             stack.append(node.right)
+        #         stack.append(node)
+        #         if node.left:
+        #             stack.append(node.left)
+        #         else:
+        #             stack.pop()
+                
+            # for i in range(len(stack)):
+            #     print(stack[i].val)
+        
+        
+        
+        
+        
 
 
 
@@ -30,7 +66,7 @@ if __name__ == '__main__':
     input_Str = str('hello')
     # input_List = []
     input_List = TreeNode(1)
-    input_List.left = TreeNode(2)
+    input_List.left = TreeNode(2,TreeNode(4),TreeNode(5))
     input_List.right = TreeNode(3)
     
     result = solu.recoverTree(input_List)
